60776e39b3fb83505d1fef3400062e2262e3e394
Brenton Forwood <forwoodb77@gmail.com>
2026-05-26 23:22:48 -0400

    Retrieved stock info in edit page

--- app/components/StockForm.tsx
+++ app/components/StockForm.tsx
@@ -1,18 +1,32 @@
 interface StockFormProps {
   formAction: (formData: FormData) => Promise<void>;
+  ticker: string;
+  company: string;
 }
 
-const StockForm = ({ formAction }: StockFormProps) => {
+const StockForm = ({ formAction, ticker, company }: StockFormProps) => {
   return (
     <form
       action={formAction}
       className="flex justify-between items-center w-[95%] mx-auto"
     >
       <label htmlFor="ticker">Ticker: </label>
-      <input type="text" name="ticker" id="ticker" className="input mr-4" />
+      <input
+        type="text"
+        name="ticker"
+        id="ticker"
+        defaultValue={ticker}
+        className="input mr-4"
+      />
 
       <label htmlFor="company">Company: </label>
-      <input type="text" name="company" id="company" className="input mr-4" />
+      <input
+        type="text"
+        name="company"
+        id="company"
+        defaultValue={company}
+        className="input mr-4"
+      />
 
       <button className="btn">Add Stock</button>
     </form>
--- app/dashboard/stocks/edit/[id]/page.tsx
+++ app/dashboard/stocks/edit/[id]/page.tsx
@@ -1,7 +1,19 @@
 import StockForm from "@/app/components/StockForm";
 import { connectDb } from "@/app/lib/mongodb";
+import Stock from "@/app/models/Stock";
 
-const EditStockPage = () => {
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+const EditStockPage = async ({ params }: PageProps) => {
+  // Get id from url params
+  const { id } = await params;
+
+  // Get stock to edit
+  const data = await Stock.findOne({ _id: id }).lean();
+  const stock = JSON.parse(JSON.stringify(data));
+
   const updateStockAction = async (formData: FormData) => {
     "use server";
     await connectDb();
@@ -11,7 +23,11 @@ const EditStockPage = () => {
   return (
     <div>
       <h1>Edit Stock</h1>
-      <StockForm formAction={updateStockAction} />
+      <StockForm
+        formAction={updateStockAction}
+        ticker={stock.ticker}
+        company={stock.company}
+      />
     </div>
   );
 };
