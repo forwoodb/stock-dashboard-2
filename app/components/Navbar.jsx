88341d8979d6ebc172201a534fb6afcbfa0e42dd
Brenton Forwood <forwoodb77@gmail.com>
2026-06-20 15:40:47 -0400

    Changed titles to 'Stock Dashboard' in navbar and meta data in layout.tsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">AuthStarter</a>
+        <a className="btn btn-ghost text-xl">Stock Dashboard</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
