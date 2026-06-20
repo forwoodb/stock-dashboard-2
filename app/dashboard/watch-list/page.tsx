90580b6f28ec54702539641c4c3907551a27c8b4
Brenton Forwood <forwoodb77@gmail.com>
2026-06-20 15:20:16 -0400

    Added csvRow type, added csv data columnsto table in watch list page

--- app/dashboard/watch-list/page.tsx
+++ app/dashboard/watch-list/page.tsx
@@ -4,6 +4,18 @@ import Stock from "@/app/models/Stock";
 import fs from "fs";
 import { parse } from "csv-parse/sync";
 
+interface csvRow {
+  ticker: string;
+  Time: string;
+  Close: number;
+  "5D": number;
+  "10D": number;
+  "20D": number;
+  "50D": number;
+  "100D": number;
+  "200D": number;
+}
+
 const WatchlistPage = async () => {
   await connectDb();
 
@@ -13,7 +25,7 @@ const WatchlistPage = async () => {
   // Get CSV data
   const csv = fs.readFileSync("csv_data.csv", "utf-8");
 
-  const stockData = parse(csv, { columns: true });
+  const stockData = parse(csv, { columns: true }) as csvRow[];
 
   const merge = stocks.map((stock) => {
     const csvRow = stockData.find((i) => {
@@ -21,7 +33,6 @@ const WatchlistPage = async () => {
     });
     return { ...stock, ...csvRow };
   });
-  console.log(merge);
 
   return (
     <div>
@@ -30,6 +41,14 @@ const WatchlistPage = async () => {
         <thead>
           <tr>
             <th>Ticker</th>
+            <th>Time</th>
+            <th>Close</th>
+            <th>5D</th>
+            <th>10D</th>
+            <th>20D</th>
+            <th>50D</th>
+            <th>100D</th>
+            <th>200D</th>
           </tr>
         </thead>
         <tbody>
@@ -37,7 +56,14 @@ const WatchlistPage = async () => {
             return (
               <tr key={stock._id}>
                 <td>{stock.ticker}</td>
+                <td>{stock.Time}</td>
+                <td>{stock.Close}</td>
+                <td>{stock["5D"]}</td>
                 <td>{stock["10D"]}</td>
+                <td>{stock["20D"]}</td>
+                <td>{stock["50D"]}</td>
+                <td>{stock["100D"]}</td>
+                <td>{stock["200D"]}</td>
               </tr>
             );
           })}
