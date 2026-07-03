3a232feff17593e5db3e760e5f0e30e1d4c47e4c
Brenton Forwood <forwoodb77@gmail.com>
2026-07-03 04:37:49 -0400

    Created EditPageProps type for the edit position page, imported id from params, imported stock data from id in edit page

--- app/dashboard/edit-stock/[id]/page.tsx
+++ app/dashboard/edit-stock/[id]/page.tsx
@@ -1,11 +1,24 @@
-const EditStockPage = () => {
+import { connectDb } from "@/app/lib/mongodb";
+import Stock from "@/app/models/Stock";
+
+type EditPageProps = {
+  params: Promise<{ id: string }>;
+};
+
+const EditStockPage = async ({ params }: EditPageProps) => {
+  await connectDb();
+
+  const { id } = await params;
+
+  const data = await Stock.findOne({ _id: id }).lean();
+  const stock = JSON.parse(JSON.stringify(data));
+
+  console.log(stock);
+
   return (
     <main>
       <h1>Edit Stock Page</h1>
       <form action="">
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
         <input type="text" />
         <button>Update</button>
       </form>
