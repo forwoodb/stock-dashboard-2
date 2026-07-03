49b03a45f79c16ecfaca318860eb7c1349a4f638
Brenton Forwood <forwoodb77@gmail.com>
2026-07-03 04:53:37 -0400

    Created form to update stock info

--- app/dashboard/edit-stock/[id]/page.tsx
+++ app/dashboard/edit-stock/[id]/page.tsx
@@ -19,6 +19,30 @@ const EditStockPage = async ({ params }: EditPageProps) => {
     <main>
       <h1>Edit Stock Page</h1>
       <form action="">
+        <label className="floating-label">
+          <span>Ticker</span>
+          <input type="text" placeholder="Ticker" className="input input-md" />
+        </label>
+        <label className="floating-label">
+          <span>Company</span>
+          <input type="text" placeholder="Company" className="input input-md" />
+        </label>
+        <label className="floating-label">
+          <span>Position Size</span>
+          <input
+            type="text"
+            placeholder="Position Size"
+            className="input input-md"
+          />
+        </label>
+        <label className="floating-label">
+          <span>Average Cost</span>
+          <input
+            type="text"
+            placeholder="Average Cost"
+            className="input input-md"
+          />
+        </label>
         <input type="text" />
         <button>Update</button>
       </form>
