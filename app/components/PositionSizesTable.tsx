b3c4991c5d4607e0b2b43982b222ac49fce6f3b6
Brenton Forwood <forwoodb77@gmail.com>
2026-07-03 13:23:36 -0400

    Cut position sizes table from page, created PositionSizesTable component, import component into position sizes page

---
+++ app/components/PositionSizesTable.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+
+const PositionSizesTable = ({ data, serverAction }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Ticker</th>
+          <th>Time</th>
+          <th>Close</th>
+          <th>5D</th>
+          <th>10D</th>
+          <th>20D</th>
+          <th>50D</th>
+          <th>100D</th>
+          <th>200D</th>
+          <th>AvgCost</th>
+          <th>PosSize</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((stock) => {
+          return (
+            <tr key={stock._id}>
+              <td>{stock.ticker}</td>
+              <td>{stock.Time}</td>
+              <td>{stock.Close}</td>
+              <td>{stock["5D"]}</td>
+              <td>{stock["10D"]}</td>
+              <td>{stock["20D"]}</td>
+              <td>{stock["50D"]}</td>
+              <td>{stock["100D"]}</td>
+              <td>{stock["200D"]}</td>
+              <td>{stock.averageCost}</td>
+              <td>{stock.positionSize}</td>
+              <td>
+                <form action={serverAction}>
+                  <input type="hidden" name="id" value={stock._id} />
+                  <button className="btn">Watch</button>
+                </form>
+              </td>
+              <td>
+                <Link
+                  href={`/dashboard/edit-stock/${stock._id}`}
+                  className="btn"
+                >
+                  Edit
+                </Link>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default PositionSizesTable;
--- app/dashboard/position-sizes/page.tsx
+++ app/dashboard/position-sizes/page.tsx
@@ -3,9 +3,9 @@ import Stock from "@/app/models/Stock";
 import { revalidatePath } from "next/cache";
 import { mergeCSVData } from "@/app/lib/functions";
 import { StockInfoType, StockType } from "@/app/lib/types";
-import Link from "next/link";
 import AccountBalanceForm from "@/app/components/AccountBalanceForm";
 import StopLossForm from "@/app/components/StopLossForm";
+import PositionSizesTable from "@/app/components/PositionSizesTable";
 
 const PositionsPage = async () => {
   await connectDb();
@@ -33,56 +33,7 @@ const PositionsPage = async () => {
       <h1>Position Sizes Page</h1>
       <AccountBalanceForm />
       <StopLossForm />
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Ticker</th>
-            <th>Time</th>
-            <th>Close</th>
-            <th>5D</th>
-            <th>10D</th>
-            <th>20D</th>
-            <th>50D</th>
-            <th>100D</th>
-            <th>200D</th>
-            <th>AvgCost</th>
-            <th>PosSize</th>
-          </tr>
-        </thead>
-        <tbody>
-          {merge.map((stock) => {
-            return (
-              <tr key={stock._id}>
-                <td>{stock.ticker}</td>
-                <td>{stock.Time}</td>
-                <td>{stock.Close}</td>
-                <td>{stock["5D"]}</td>
-                <td>{stock["10D"]}</td>
-                <td>{stock["20D"]}</td>
-                <td>{stock["50D"]}</td>
-                <td>{stock["100D"]}</td>
-                <td>{stock["200D"]}</td>
-                <td>{stock.averageCost}</td>
-                <td>{stock.positionSize}</td>
-                <td>
-                  <form action={toWatchList}>
-                    <input type="hidden" name="id" value={stock._id} />
-                    <button className="btn">Watch</button>
-                  </form>
-                </td>
-                <td>
-                  <Link
-                    href={`/dashboard/edit-stock/${stock._id}`}
-                    className="btn"
-                  >
-                    Edit
-                  </Link>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <PositionSizesTable data={merge} serverAction={toWatchList} />
     </main>
   );
 };
