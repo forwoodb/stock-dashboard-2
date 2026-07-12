f3ee3a06c0fe09e437b3f97bfd33156d464b6f50
Brenton Forwood <forwoodb77@gmail.com>
2026-07-12 15:45:08 -0400

    Changed conditional rendering of text color to entire row

--- app/components/WatchListTable.tsx
+++ app/components/WatchListTable.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import { StockInfoType } from "../lib/types";
 
 interface WatchListProps {
@@ -7,6 +9,12 @@ interface WatchListProps {
 }
 
 const WatchListTable = ({ mode, data, formAction }: WatchListProps) => {
+  const [selectedMA, setSelectedMA] = useState("10D");
+
+  const selectMA = (id) => {
+    console.log("click");
+  };
+
   return (
     <table className="table">
       <thead>
@@ -14,27 +22,39 @@ const WatchListTable = ({ mode, data, formAction }: WatchListProps) => {
           <th>Ticker</th>
           <th>Time</th>
           <th>Close</th>
-          <th>5D</th>
-          <th>10D</th>
-          <th>20D</th>
-          <th>50D</th>
-          <th>100D</th>
-          <th>200D</th>
+          <th id="5D" onClick={selectMA}>
+            5D
+          </th>
+          <th id="10D" onClick={selectMA}>
+            10D
+          </th>
+          <th id="20D" onClick={selectMA}>
+            20D
+          </th>
+          <th id="50D" onClick={selectMA}>
+            50D
+          </th>
+          <th id="100D" onClick={selectMA}>
+            100D
+          </th>
+          <th id="200D" onClick={selectMA}>
+            200D
+          </th>
           <th>Cl&gt;MA</th>
         </tr>
       </thead>
       <tbody>
         {data.map((stock) => {
-          const pctAbvMa = stock.Close - stock["10D"];
+          const pctAbvMa = stock.Close - stock[selectedMA];
           let textColor;
           if (pctAbvMa < 0) {
             textColor = "red";
           }
           return (
-            <tr key={stock._id}>
+            <tr key={stock._id} className={pctAbvMa < 0 && `text-red-600`}>
               <td>{stock.ticker}</td>
               <td>{stock.Time}</td>
-              <td className={pctAbvMa < 0 && `text-red-600`}>{stock.Close}</td>
+              <td>{stock.Close}</td>
               <td>{stock["5D"]}</td>
               <td>{stock["10D"]}</td>
               <td>{stock["20D"]}</td>
@@ -42,9 +62,7 @@ const WatchListTable = ({ mode, data, formAction }: WatchListProps) => {
               <td>{stock["100D"]}</td>
               <td>{stock["200D"]}</td>
               {/* <td className={pctAbvMa < 0 && `text-red`}> */}
-              <td className={pctAbvMa < 0 && `text-red-600`}>
-                {pctAbvMa.toFixed(2)}%
-              </td>
+              <td>{pctAbvMa.toFixed(2)}%</td>
               <td>
                 <form action={formAction}>
                   <input type="hidden" name="id" value={stock._id} />
