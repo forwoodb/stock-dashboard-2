95dd7fea57f2b85da7f94e5767e5b6a7ed97897f
Brenton Forwood <forwoodb77@gmail.com>
2026-07-20 02:35:16 -0400

    Removed label from data output

--- app/api/stock-data/route.ts
+++ app/api/stock-data/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const res = await fetch("http://localhost:8000/get_stocks");
   const data = await res.json();
-  console.log(data);
+  // console.log(data);
 
-  return NextResponse.json({ python_data: data });
+  return NextResponse.json(data);
 }
