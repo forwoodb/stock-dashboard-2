9eae77f4df98b89cd71bcf1e4bc01455656fd459
Brenton Forwood <forwoodb77@gmail.com>
2026-07-20 02:21:22 -0400

    Created GET route to connect to FastAPI yahoo.py

--- app/api/stock-data/route.ts
+++ app/api/stock-data/route.ts
@@ -3,5 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const res = await fetch("http://localhost:8000/get_stocks");
   const data = await res.json();
+  console.log(data);
+
   return NextResponse.json({ python_data: data });
 }
