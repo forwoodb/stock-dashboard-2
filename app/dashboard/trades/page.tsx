d9aa065cff76191e6e2ade5f929e07e62b487502
Brenton Forwood <forwoodb77@gmail.com>
2026-07-31 04:11:18 -0400

    Updated data read from mongoose object to JSON object

--- app/dashboard/trades/page.tsx
+++ app/dashboard/trades/page.tsx
@@ -4,17 +4,27 @@ import Transaction from "@/app/models/Transaction";
 const TradesPage = async () => {
   await connectDb();
 
-  const trades = await Transaction.find({});
+  const data = await Transaction.find({}).lean();
+  const trades = JSON.parse(JSON.stringify(data));
 
   return (
     <table className="table">
-      <thead></thead>
+      <thead>
+        <tr>
+          <th>Ticker</th>
+          <th>Type</th>
+          <th>Pos Size</th>
+          <th>5D</th>
+        </tr>
+      </thead>
       <tbody>
         {trades.map((trade) => {
           return (
             <tr key={trade._id}>
               <td>{trade.ticker}</td>
               <td>{trade.type}</td>
+              <td>{trade.positionSize}</td>
+              <td>{trade.fiveDayAvg}</td>
             </tr>
           );
         })}
