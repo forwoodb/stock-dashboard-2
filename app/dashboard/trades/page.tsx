a9504ccd0c7620de5443789ec55a6348370e34dd
Brenton Forwood <forwoodb77@gmail.com>
2026-07-31 05:30:57 -0400

    Added more data columns to trades table

--- app/dashboard/trades/page.tsx
+++ app/dashboard/trades/page.tsx
@@ -14,9 +14,15 @@ const TradesPage = async () => {
         <tr>
           <th>Ticker</th>
           <th>Type</th>
+          <th>Price</th>
           <th>Pos Size</th>
           <th>$ Amt</th>
           <th>5D</th>
+          <th>10D</th>
+          <th>20D</th>
+          <th>50D</th>
+          <th>100D</th>
+          <th>200D</th>
         </tr>
       </thead>
       <tbody>
@@ -25,9 +31,15 @@ const TradesPage = async () => {
             <tr key={trade._id}>
               <td>{trade.ticker}</td>
               <td>{trade.type}</td>
+              <td>{trade.price}</td>
               <td>{trade.positionSize}</td>
               <td>{trade.dollarAmount}</td>
               <td>{trade.fiveDayAvg}</td>
+              <td>{trade.tenDayAvg}</td>
+              <td>{trade.twentyDayAvg}</td>
+              <td>{trade.fiftyDayAvg}</td>
+              <td>{trade.oneHundredDayAvg}</td>
+              <td>{trade.twoHundredDayAvg}</td>
             </tr>
           );
         })}
