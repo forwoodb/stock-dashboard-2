6ab700e0991692e84b38888c0ddef3777f1c2ba2
Brenton Forwood <forwoodb77@gmail.com>
2026-08-25 02:20:16 -0400

    Added data retrieval for the Trade and populated form input for editing

--- app/dashboard/trades/[id]/page.tsx
+++ app/dashboard/trades/[id]/page.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import Transaction from "@/app/models/Transaction";
 
-const EditTradePage = () => {
+interface PageProps {
+  params: Promise<{ id: string }>;
+}
+
+const EditTradePage = async ({ params }: PageProps) => {
+  const { id } = await params;
+
+  const data = await Transaction.findOne({ _id: id }).lean();
+  const trade = JSON.parse(JSON.stringify(data));
+  console.log(trade);
+
   return (
     <main>
       <h1>Edit Trade</h1>
       <form action="">
         <label className="floating-label">
           <span>Ticker</span>
-          <input type="text" placeholder="Ticker" className="input input-md" />
+          <input
+            type="text"
+            placeholder="Ticker"
+            defaultValue={trade.ticker}
+            className="input input-md"
+          />
         </label>
       </form>
     </main>
