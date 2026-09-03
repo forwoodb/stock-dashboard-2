7402af7447f5835a30b34ca871ca275814c9243c
Brenton Forwood <forwoodb77@gmail.com>
2026-09-03 07:32:10 -0400

    Created functionality to ensure user wants to delete trade in DeleteTrade component by adding state and conditional rendering to buttons

--- app/components/DeleteTrade.tsx
+++ app/components/DeleteTrade.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 import { Trade } from "../lib/types";
 
 interface DeleteTradeProps {
@@ -7,12 +8,25 @@ interface DeleteTradeProps {
   trade: Trade;
 }
 const DeleteTrade = ({ deleteTradeAction, trade }: DeleteTradeProps) => {
+  const [del, setDelete] = useState(false);
+
   return (
     <div>
-      <p>Are you sure?</p>
-      <form action={deleteTradeAction.bind(null, trade._id)}>
-        <button className="btn">Delete</button>
-      </form>
+      <button
+        onClick={() => setDelete(true)}
+        className={`btn ${del && "hidden"}`}
+      >
+        Delete
+      </button>
+      <div className={`${del === false && "hidden"}`}>
+        <p>Are you sure?</p>
+        <button onClick={() => setDelete(false)} className="btn">
+          No
+        </button>
+        <form action={deleteTradeAction.bind(null, trade._id)}>
+          <button className="btn">Yes</button>
+        </form>
+      </div>
     </div>
   );
 };
